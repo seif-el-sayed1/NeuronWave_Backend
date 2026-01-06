58d986b0e6e8bc7fe251e70d77a6f27f4e1d127c
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-06 04:39:31 +0200

    doctor model

---
+++ models/doctor.model.js
@@ -0,0 +1,172 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const {
+    DOCTOR,
+    MEDICAL_SPECIALTIES,
+    LOGIN_TYPE_LIST,
+} = require("../utils/constants");
+const localizationSetUp = require("../utils/modelLocalizationSetUp");
+
+const doctorSchema = mongoose.Schema(
+  {
+    lang: {
+      type: String,
+      enum: ["en", "ar"],
+      default: "en"
+    },
+    loginType: {
+      type: String,
+      enum: LOGIN_TYPE_LIST,
+      default: "email"
+    },
+    fullName: {
+      type: String,
+      trim: true
+    },
+    role: {
+      type: String,
+      default: DOCTOR
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true
+    },
+    phone: {
+      type: String,
+      trim: true
+    },
+    medicalSpecialty: {
+        type: String,
+        enum: MEDICAL_SPECIALTIES,
+        required: true
+    },
+    medicalLicenseNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    hospital: {
+        type: String,
+        trim: true
+    },
+    // Password
+    password: {
+      type: String,
+      minLength: [6, "Too short password"],
+    },
+    passwordChangedAt: Date,
+    passwordResetCode: String,
+    passwordResetCodeExp: Date,
+    passwordResetCodeVerified: Boolean,
+    // Verification
+    verificationCode: String,
+    verificationCodeExp: Date,
+    verificationCodeVerified: Boolean,
+    passwordVerificationToken: String,
+    passwordResetExpiresAt: Date,
+    isVerified: {
+      type: Boolean,
+      default: false
+    },
+    notVerifiedTime: {
+      type: Date,
+      expires: "10m"
+    },
+    // Active
+    deactivatedAt: {
+      type: Date,
+      expires: "15d"
+    },
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false
+    },
+    token: String,
+    tokenExpDate: Date,
+    notificationToken: String,
+  },
+  { timestamps: true}
+);
+doctorSchema.index({ location: "2dsphere" });
+
+// Creating a partial unique index
+doctorSchema.index(
+  { phone: 1 }, // The field to index
+  {
+    unique: true, // Enforce uniqueness
+    partialFilterExpression: { phone: { $exists: true } } // Apply uniqueness only when `phone` is not null
+  }
+);
+doctorSchema.index(
+  { email: 1 }, // The field to index
+  {
+    unique: true, // Enforce uniqueness
+    partialFilterExpression: { email: { $exists: true } } // Apply uniqueness only when `email` is not null
+  }
+);
+
+doctorSchema.methods.generateToken = async function () {
+  const tokenExpDate = new Date(); // Get the current date
+  tokenExpDate.setDate(
+    tokenExpDate.getDate() + parseInt(process.env.JWT_EXPIRATION.toString().slice(0, -1))
+  );
+  const token = jwt.sign(
+    {
+      userId: this._id,
+      role: DOCTOR,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRATION
+    }
+  );
+  // Save the generated token to the database
+  this.token = token;
+  this.tokenExpDate = tokenExpDate;
+  this.updatePassword = false; // to skip hashing the password
+
+  await this.save();
+
+  return { token, tokenExpDate };
+};
+
+doctorSchema.methods.comparePassword = async function (password) {
+  if (this.loginType !== "email") {
+    if (password) return false;
+  } else return await bcrypt.compare(password, this.password);
+};
+
+//runs on changing the password to change the time that password changedAt
+doctorSchema.pre("save", async function (next) {
+  if (!this.password) return next();
+  if (!this.isModified("password")) return next();
+  //hashing Password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+doctorSchema.methods.generatePasswordVerificationToken = async function (session) {
+  // Generate a random token
+  const passwordCreationToken = crypto.randomBytes(32).toString("hex");
+  // Hash the token before storing it in the DB
+  const hashedToken = crypto.createHash("sha256").update(passwordCreationToken).digest("hex");
+
+  // Set the token and expiration in the schema
+  this.passwordVerificationToken = hashedToken;
+  this.passwordResetExpiresAt = Date.now() + 10 * 60 * 1000; // 10 minutes expiry
+  // Save the admin document with the token
+  await this.save({ validateBeforeSave: false });
+
+  return passwordCreationToken;
+};
+
+module.exports = mongoose.model("Doctor", doctorSchema);
