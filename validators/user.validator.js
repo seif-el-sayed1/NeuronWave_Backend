7933280541302d3f158b897e5077d3d927842599
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-06 04:37:39 +0200

    user validator

---
+++ validators/user.validator.js
@@ -0,0 +1,234 @@
+const Joi = require("joi");
+const asyncHandler = require("express-async-handler");
+const joiErrorHandler = require("./joiErrorHandler");
+const {
+  phoneNumberValidator,
+} = require("./validatorComponents");
+const ApiError = require("../utils/ApiError");
+const { translate } = require("../utils/translation");
+const User = require("../models/user.model");
+const {
+  GENDER_LIST_EN,
+  GENDER_LIST_AR,
+  LOGIN_TYPE_PLATFORM_LIST,
+  LANGS,
+} = require("../utils/constants");
+const {
+  checkIfPhoneStartsWithPlus2,
+} = require("../middlewares/phoneNumberChecker.middleware");
+
+/**
+ * UserValidator class for validating user registration requests.
+ *
+ * This class contains methods for validating user data during the registration process using Joi and additional checks.
+ */
+class UserValidator {
+  validateRegisterUser = asyncHandler(async (req, res, next) => {
+    const schema = Joi.object({
+      fullName: Joi.string()
+        .when("loginType", {
+          is: Joi.valid(...LOGIN_TYPE_PLATFORM_LIST),
+          then: Joi.optional().allow(""),
+          otherwise: Joi.required(),
+        })
+        .min(2)
+        .max(32)
+        .messages({ "any.required": "Full Name is required" }),
+
+      email: Joi.string().email().required().messages({
+        "any.required": "Email is required",
+        "string.email": "Invalid Email Address",
+      }),
+
+      phone: Joi.string().custom(phoneNumberValidator).required().messages({
+        "any.required": "Phone is required",
+        "string.pattern.base": "Invalid Phone Number",
+      }),
+
+      genderEn: Joi.string()
+        .valid(...GENDER_LIST_EN)
+        .required()
+        .messages({ "any.required": "Gender is required" }),
+
+      genderAr: Joi.string()
+        .valid(...GENDER_LIST_AR)
+        .optional(),
+
+      emergencyContact: Joi.string().custom(phoneNumberValidator).required().messages({
+        "any.required": "Emergency Contact is required",
+        "string.pattern.base": "Invalid Phone Number",
+      }),
+
+      dateOfBirth: Joi.date()
+        .required()
+        .messages({ "any.required": "Date of Birth is required" }),
+
+      loginType: Joi.string().optional(),
+
+      password: Joi.string()
+        .min(6)
+        .when("loginType", { is: Joi.exist(), then: Joi.optional() })
+        .messages({
+          "string.min": "Password must be at least 6 characters",
+          "any.required": "Password is required",
+        }),
+
+      confirmPassword: Joi.string()
+        .valid(Joi.ref("password"))
+        .when("loginType", { is: Joi.exist(), then: Joi.optional() })
+        .messages({
+          "any.required": "Confirm Password is required",
+          "any.only": "Passwords do not match",
+        }),
+
+      notificationToken: Joi.string().optional(),
+    });
+
+
+    joiErrorHandler(schema, req);
+    checkIfPhoneStartsWithPlus2(req);
+    next();
+  });
+
+  validateUpdateUser = asyncHandler(async (req, res, next) => {
+    const schema = Joi.object({
+      fullName: Joi.string().optional().min(2).max(32),
+      dateOfBirth: Joi.date().optional(),
+      phone: Joi.string().custom(phoneNumberValidator).optional().messages({
+        "string.pattern.base":
+          "Phone number must start with '0' and contain exactly 11 digits",
+        "any.required": "Phone number is required",
+      }),
+      email: Joi.string().email().optional(),
+      genderEn: Joi.string()
+        .valid(...GENDER_LIST_EN)
+        .optional(),
+      genderAr: Joi.string()
+        .valid(...GENDER_LIST_AR)
+        .optional(),
+      dateOfBirth: Joi.date().optional(),
+      emergencyContact: Joi.string().custom(phoneNumberValidator).optional().messages({
+        "string.pattern.base":
+        "Emergency contact number must start with '0' and contain exactly 11 digits",
+        "any.required": "Emergency contact number is required",
+      }),
+      // Validate location using the defined schema
+    });
+    joiErrorHandler(schema, req);
+    if (genderEn) {
+      switch (genderEn.toLowerCase()) {
+        case "male":
+          req.body.genderAr = "ذكر";
+          break;
+        case "female":
+          req.body.genderAr = "أنثى";
+          break;
+      }
+    }
+    checkIfPhoneStartsWithPlus2(req);
+    let { phone, email } = req.body;
+    if (phone) {
+      if (phone !== req.user.phone) {
+        let user = await User.findOne({
+          $or: [{ phone }, { unverifiedPhone: phone }],
+          _id: { $ne: req.user._id },
+        });
+        if (user)
+          return next(
+            new ApiError(
+              translate("Duplicated Phone Number", req.headers.lang),
+              400
+            )
+          );
+      } else
+        return next(
+          new ApiError(
+            translate(
+              "This Phone Number Has Been Verified Before",
+              req.headers.lang
+            ),
+            400
+          )
+        );
+    } else if (email) {
+      if (email !== req.user.email) {
+        let user = await User.findOne({
+          $or: [{ email }, { unverifiedEmail: email }],
+          _id: { $ne: req.user._id },
+        });
+        if (user)
+          return next(
+            new ApiError(translate("Duplicated Email", req.headers.lang), 400)
+          );
+      } else
+        return next(
+          new ApiError(
+            translate("This Email Has Been Verified Before", req.headers.lang),
+            400
+          )
+        );
+    }
+    next();
+  });
+
+  validateUserBlock = asyncHandler(async (req, res, next) => {
+    let { id } = req.params;
+    if (id.toString() === req.userId.toString())
+      return next(
+        new ApiError(
+          translate("You Can't Block Yourself", req.headers.lang),
+          400
+        )
+      );
+    let user = await User.findById(id, null, { skipPopulation: true });
+    if (!user)
+      return next(
+        new ApiError(translate("User Not Found!", req.headers.lang), 404)
+      );
+    next();
+  });
+
+  checkUserExistence = asyncHandler(async (req, res, next) => {
+    let { id } = req.params;
+    let user = await User.findById(id);
+    if (!user)
+      return next(
+        new ApiError(translate("User Not Found!", req.headers.lang), 404)
+      );
+    req.requestedUser = user; // this is the intendedUser to add action on him
+    next();
+  });
+
+  validateDeleteImages = asyncHandler(async (req, res, next) => {
+    const schema = Joi.object({
+      deletedPhotos: Joi.array().items(Joi.string()).required(), // Limiting images to a maximum of 4,
+      type: Joi.string()
+        .required()
+        .valid(...USER_TYPE_LIST),
+    });
+    joiErrorHandler(schema, req);
+
+    let { id: _id } = req.params;
+    let requestedUser = await User.findById(_id, null, {
+      skipPopulation: true,
+    });
+    if (!requestedUser)
+      return next(
+        new ApiError(translate("User Not Found!", req.headers.lang), 404)
+      );
+    req.requestedUser = requestedUser;
+    next();
+  });
+
+  validateLanguageUpdate = asyncHandler(async (req, res, next) => {
+    const schema = Joi.object({
+      lang: Joi.string()
+        .required()
+        .valid(...LANGS),
+    });
+    joiErrorHandler(schema, req);
+    next();
+  });
+}
+
+module.exports = new UserValidator();
