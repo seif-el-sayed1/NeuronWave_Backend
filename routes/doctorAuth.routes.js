c48aa71cd060d04250fc112e6e7dd0798cea8c4a
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-06 04:38:58 +0200

    doctor auth routes

---
+++ routes/doctorAuth.routes.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const { DOCTOR } = require("../utils/constants");
+
+// Middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+// Classes
+const DoctorAuthController = require("../controllers/doctorAuth.controller");
+const DoctorResetPasswordController = require("../controllers/doctorResetPassword.controllers");
+const DoctorValidator = require("../validators/doctor.validator");
+const GlobalValidator = require("../validators/global.validator");
+
+// Router`
+const router = express.Router();
+
+// Auth Routes
+router.route("/register").post(
+  DoctorValidator.validateRegisterDoctor,
+  DoctorAuthController.doctorRegister
+);
+
+router.route("/login").post(GlobalValidator.validateLogin, DoctorAuthController.doctorLogin);
+
+router.route("/verify-account").post(DoctorAuthController.doctorVerifyAccount);
+//  Forgot Password Routes
+router
+  .route("/forgot-password")
+  .post(DoctorValidator.forgetPasswordValidator, DoctorResetPasswordController.forgotPassword);
+router
+  .route("/verify-reset-code")
+  .post(DoctorValidator.resetPasswordCodeValidator, DoctorResetPasswordController.verifyResetCode);
+router
+  .route("/reset-password")
+  .patch(DoctorValidator.resetPasswordValidator, DoctorResetPasswordController.resetPassword);
+
+router.patch(
+  "/change-password",
+  protect,
+  allowedTo(DOCTOR),
+  GlobalValidator.validateChangePassword,
+  DoctorAuthController.updateLoggedDoctorPassword
+);
+
+router.post("/verify-otp", DoctorAuthController.verifyOtp);
+router.post("/send-otp", GlobalValidator.sendOtpValidator, DoctorAuthController.sendOtp);
+router.post("/log-out", protect, allowedTo(DOCTOR), DoctorAuthController.logOut);
+
+module.exports = router;
