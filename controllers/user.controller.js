b158cd075de37919ec2494d998aafee59853600f
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-07 03:52:19 +0200

    update my profile (Patient)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,6 +18,26 @@ class UserController {
         });
     });
     
+    //@desc Update me
+    //@route PUT /api/v1/users/:id
+    //@access Private
+    updateMe = asyncHandler(async (req, res, next) => {
+        const oldUser = await User.findById(req.user._id);
+        if (!oldUser) return next(new ApiError("User not found", 404));
+        
+        const user = await User.findByIdAndUpdate(req.user._id, req.body, {
+            new: true,
+            runValidators: true,
+        }).select("fullName phone email  createdAt role dateOfBirth emergencyContact medicalId");
+        if (!user) return next(new ApiError("User not found", 404));
+
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    });
+
+    
 }
 
 module.exports = new UserController();
