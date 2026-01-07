605ea3b8e8d85c754214c62c0a9608660659f845
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-07 03:51:31 +0200

    get my profile (patient)

---
+++ controllers/user.controller.js
@@ -0,0 +1,23 @@
+const asyncHandler = require("express-async-handler");
+const ApiError = require("../utils/ApiError");
+const User = require("../models/user.model");
+
+class UserController {
+    //@desc Get My Profile
+    //@route GET /api/v1/users/me
+    //@access Private
+    getMyProfile = asyncHandler(async (req, res, next) => {
+        const user = await User.findById(req.user._id)
+                .select("fullName phone email  createdAt role dateOfBirth emergencyContact medicalId");
+
+        if (!user) return next(new ApiError("User not found", 404));
+
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    });
+    
+}
+
+module.exports = new UserController();
