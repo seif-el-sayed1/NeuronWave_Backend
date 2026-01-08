36c1f5af37a75a81e83ea165d9af4176b0a6ef3c
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-08 04:04:37 +0200

    enhance html for emails

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -4,14 +4,10 @@ class EmailController {
   userVerificationEmail = async (code, emailAddress) => {
 
     const html = generateHTML({
-      link: process.env.LANDING_URL,
       emailTitle: "Verify Your user Account",
       emailSubTitle: "Use the code below to verify your email address.",
       btnText: code,
-      belowText: "Visit our website:",
-      belowLink: process.env.LANDING_URL,
       footerNote: `You received this email because you have registered on ${process.env.APP_NAME}. If you did not initiate this action, please ignore this email.`,
-      footerLink: process.env.APP_NAME
     });
     await sendEmail({
       email: emailAddress,
@@ -23,14 +19,10 @@ class EmailController {
   userResetPasswordEmail = async (emailAddress, code) => {
 
     const html = generateHTML({
-      link: process.env.LANDING_URL,
       emailTitle: "Forgot Password Account",
       emailSubTitle: "Use the code below to reset your email password.",
       btnText: code,
-      belowText: "Visit our website:",
-      belowLink: process.env.LANDING_URL,
       footerNote: `You received this email because you have registered on ${process.env.APP_NAME}. If you did not initiate this action, please ignore this email.`,
-      footerLink: process.env.APP_NAME
     });
     await sendEmail({
       email: emailAddress,
@@ -39,19 +31,6 @@ class EmailController {
     });
   };
 
-  userForgotPasswordEmail = async (code, emailAddress) => {
-    const html = generateHTML({
-      emailTitle: `Reset your ${process.env.APP_NAME} account password`,
-      emailSubTitle: "Use the code below to reset your account password.",
-      btnText: code,
-      footerNote: `You are receiving this email because a request to reset the password for your ${process.env.APP_NAME} account has been initiated. If you did not initiate this action, please disregard this message.`
-    });
-    await sendEmail({
-      email: emailAddress,
-      subject: `${process.env.APP_NAME} reset account password`,
-      html: html
-    });
-  };
 
 }
 
