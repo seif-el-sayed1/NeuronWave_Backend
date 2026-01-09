6949c4b9d070a16e4612dda1e81ab56f54544489
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-09 14:11:42 +0200

    appointment model

---
+++ models/appointment.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { APPOINTMENT_TYPES, APPOINTMENT_STATUS } = require('../utils/constants');
+
+const appointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true
+    },
+    date: {
+        type: Date,
+    },
+    time: {
+        type: String,
+    },
+    type: {
+        type: String,
+        enum: APPOINTMENT_TYPES,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: APPOINTMENT_STATUS,
+        default: 'pending'
+    },
+    rejectionReason: {
+        type: String,
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
