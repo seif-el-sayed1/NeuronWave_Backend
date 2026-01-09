6c93e65a8a5190c1aa1e5c174c46bc55e5f2a804
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-09 14:17:20 +0200

    appointment routes

---
+++ routes/appointment.routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const { USER, DOCTOR } = require("../utils/constants");
+
+// Middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+// Classes
+const AppointmentController = require("../controllers/appointment.controller");
+const AppointmentValidator = require("../validators/appointment.validator");
+
+// Router
+const router = express.Router();
+
+router.route("/")
+    .post( 
+        protect, 
+        allowedTo(USER, DOCTOR), 
+        AppointmentValidator.validateCreateAppointment, 
+        AppointmentController.createAppointment
+    ).get(
+        protect, 
+        allowedTo(USER), 
+        AppointmentController.getMyAppointments
+    )
+
+router.route("/doctor")
+    .get( 
+        protect, 
+        allowedTo(DOCTOR), 
+        AppointmentController.getDoctorAppointments
+    );
+
+router.route("/:id/status")
+    .patch(
+        protect,
+        allowedTo(USER, DOCTOR),
+        AppointmentController.changeAppointmentStatus
+    );
+
+router.route("/:id")
+    .patch( 
+        protect, 
+        allowedTo(DOCTOR), 
+        AppointmentValidator.validateUpdateAppointmentTime, 
+        AppointmentController.updateAppointment
+    ).delete( 
+        protect, 
+        allowedTo(USER, DOCTOR), 
+        AppointmentController.deleteAppointment
+    );
+
+
+module.exports = router;
