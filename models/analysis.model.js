490eb9e2bfaca5bd3ccbc29eedcd7d23e5cf3fda
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-10 04:54:52 +0200

    analysis model

---
+++ models/analysis.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const analysisSchema = mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["request", "byDoctor"],
+        default: "request"
+    },
+    status: {
+        type: String,
+        enum: ["pending", "rejected", "approved"],
+        default: "pending"
+    },
+    media: [String],
+    modelType: {
+        type: String,
+        required: true
+    },
+    result: {
+        type: [mongoose.Schema.Types.Mixed],
+        default: []
+    },
+    consultation: {
+        type: String
+    }
+})
+module.exports = mongoose.model("Analysis", analysisSchema)
