ba328903efaf7d61e13a6564b40ccc32092564cc
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-24 19:40:05 +0200

    create admin while server is running if there is not exist in DB

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = args.port || process.env.PORT || 8000;
 // Startup
 require("./startup/logging")(app);
 require("./startup/app")(app);
+require("./startup/createSuperAdmin")();
 require("./startup/db")();
 // require("./startup/cronJobs")();
 // Server
---
+++ startup/createSuperAdmin.js
@@ -0,0 +1,19 @@
+require("colors");
+const asyncHandler = require("express-async-handler");
+const Admin = require("../models/admin.model");
+
+const createSuperAdmin = asyncHandler(async () => {
+  const admin = await Admin.findOne({ isSuperAdmin: true  });
+  if (!admin) {
+    const admin = new Admin({
+      fullName: "Super Admin",
+      email: process.env.SUPER_ADMIN_EMAIL,
+      password: process.env.SUPER_ADMIN_PASSWORD,
+      isSuperAdmin: true
+    });
+    console.log("=== Super Admin created successfully ===".green);
+    await admin.save();
+  }
+});
+
+module.exports = createSuperAdmin;
