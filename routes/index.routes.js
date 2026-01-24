686cbbe3ac42de75f9a3eb7b498c8d53079af8de
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-24 20:13:16 +0200

    UPDATE

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,23 +2,31 @@ const appRouter = require("express").Router();
 const BASE_URL = "/api/v1";
 
 const ApiError = require("../utils/ApiError");
+let adminAuthRoutes = require("./adminAuth.routes")
 let userAuthRoutes = require("./userAuth.routes")
 let doctorAuthRoutes = require("./doctorAuth.routes")
+let oAuthRoutes = require("./oAuth.routes")
 let userRoutes = require("./user.routes")
 let doctorRoutes = require("./doctor.routes")
 let appointmentRoutes = require("./appointment.routes")
 let patientRoutes = require("./patient.routes")
 let notificationRoutes = require("./notification.routes")
 let analysisRoutes = require("./analysis.routes")
+let cityRoutes = require("./city.routes")
+let countryRoutes = require("./country.routes")
 
+appRouter.use(`${BASE_URL}/admins/auth`, adminAuthRoutes);
 appRouter.use(`${BASE_URL}/users/auth`, userAuthRoutes);
 appRouter.use(`${BASE_URL}/doctors/auth`, doctorAuthRoutes);
+appRouter.use(`${BASE_URL}/oauth`, oAuthRoutes);
 appRouter.use(`${BASE_URL}/users`, userRoutes);
 appRouter.use(`${BASE_URL}/doctors`, doctorRoutes);
 appRouter.use(`${BASE_URL}/appointments`, appointmentRoutes);
 appRouter.use(`${BASE_URL}/patients`, patientRoutes);
 appRouter.use(`${BASE_URL}/notifications`, notificationRoutes);
 appRouter.use(`${BASE_URL}/analysis`, analysisRoutes);
+appRouter.use(`${BASE_URL}/cities`, cityRoutes);
+appRouter.use(`${BASE_URL}/countries`, countryRoutes);
 
 
 appRouter.get("/", (req, res) => {
