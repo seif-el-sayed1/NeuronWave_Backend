edbb3e673cfa8a9b1a0bafa341040067c0d0ffb5
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-25 05:03:23 +0200

    add hospital routes with authorization for SUPER_DOCTOR role

---
+++ routes/hospital.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const { ADMIN, SUPER_DOCTOR } = require("../utils/constants");
+
+// Middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+// Classes
+const HospitalController = require("../controllers/hospital.controller");
+const HospitalValidator = require("../validators/hospital.validator");
+
+// Router
+const router = express.Router();
+
+router.route("/")
+    .post( 
+        protect, 
+        allowedTo(SUPER_DOCTOR), 
+        HospitalValidator.validateCreateHospital, 
+        HospitalController.createHospital
+    ).get(
+        protect, 
+        allowedTo(SUPER_DOCTOR), 
+        HospitalController.getAllHospitals
+    )
+
+router.route("/:id")
+    .patch( 
+        protect, 
+        allowedTo(SUPER_DOCTOR), 
+        HospitalValidator.validateUpdateHospital, 
+        HospitalController.updateHospital
+    ).delete(
+        protect, 
+        allowedTo(SUPER_DOCTOR), 
+        HospitalController.deleteHospital
+    )
+
+module.exports = router;
