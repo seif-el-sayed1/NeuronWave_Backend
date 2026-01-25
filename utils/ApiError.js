3cba446f37eb44d327bb5d8dc81cd63099211b08
seif-el-sayed1 <seifsayed121@gmail.com>
2026-01-25 05:05:30 +0200

    refactor ApiError constructor to set default statusCode and success properties

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -1,12 +1,11 @@
 class ApiError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.isOperational = true;
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith(4) ? "Failed" : "Error";
-    this.success = false;
-    Error.captureStackTrace(this, this.constructor);
-  }
+    constructor(message, statusCode = 500) {
+        super(message);
+        this.isOperational = true;
+        this.statusCode = statusCode;
+        this.success = statusCode >= 200 && statusCode < 400;
+        Error.captureStackTrace(this, this.constructor);
+    }
 }
 
 module.exports = ApiError;
