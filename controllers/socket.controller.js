6be1adf987b2eb101f181a5edfaddc49c5e9c29d
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-17 01:37:45 +0200

    join chat

---
+++ controllers/socket.controller.js
@@ -0,0 +1,56 @@
+const User = require("../models/user.model");
+const Doctor = require("../models/doctor.model");
+const Chat = require("../models/chat.model");
+const Message = require("../models/message.model");
+const { translate } = require("../utils/translation");
+const { sendNotification } = require("../utils/sendNotification");
+
+class SocketController {
+
+  // User joins a chat room
+  // - Adds user to socket room
+  // - Marks incoming messages as delivered & read
+  // - Notifies the other participant that messages have been seen
+  joinChat = async (io, socket, userData, chatRoomUsers, { chatId }) => {
+    try {
+      if (!chatId) return socket.emit("error", "Chat ID is required");
+
+      const chat = await Chat.findById(chatId);
+      if (!chat || !chat.hasParticipant(userData._id)) {
+        return socket.emit("error", "Chat not found or unauthorized");
+      }
+
+      // Join socket.io room
+      socket.join(chatId.toString());
+
+      // Track online users per chat room
+      if (!chatRoomUsers[chatId]) chatRoomUsers[chatId] = new Set();
+      chatRoomUsers[chatId].add(userData._id.toString());
+
+      // Mark all unread incoming messages as delivered and read
+      await Message.updateMany(
+        { 
+          chat: chatId, 
+          "sender.senderId": { $ne: userData._id } 
+        },
+        { isDelivered: true, isRead: true }
+      );
+
+      // Notify the other person that their messages were seen
+      const otherParticipant = chat.getOtherParticipant(userData._id);
+      if (otherParticipant) {
+        io.to(otherParticipant.participantId.toString()).emit("messages-seen", {
+          chatId,
+          seenTime: new Date()
+        });
+      }
+    } catch (error) {
+      console.error("Error in joinChat:", error);
+      socket.emit("error", "Failed to join chat");
+    }
+  };
+
+
+}
+
+module.exports = new SocketController();
