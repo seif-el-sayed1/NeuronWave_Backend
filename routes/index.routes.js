6a98f4c7cfc0e69c2ba75dc72e0aa50b7d4501d0
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-16 15:26:43 +0200

    add chat routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,6 @@ const BASE_URL = "/api/v1";
 const ApiError = require("../utils/ApiError");
 let userAuthRoutes = require("./userAuth.routes")
 let doctorAuthRoutes = require("./doctorAuth.routes")
-// let oAuthRoutes = require("./oAuth.routes")
 let userRoutes = require("./user.routes")
 let doctorRoutes = require("./doctor.routes")
 let appointmentRoutes = require("./appointment.routes")
@@ -15,10 +14,10 @@ let countryRoutes = require("./country.routes")
 let hospitals = require("./hospital.routes")
 let superDoctorRoutes = require("./superDoctor.routes")
 let paymentRoutes = require("./payment.routes")
+let chatRoutes = require("./chat.routes")
 
 appRouter.use(`${BASE_URL}/users/auth`, userAuthRoutes);
 appRouter.use(`${BASE_URL}/doctors/auth`, doctorAuthRoutes);
-// appRouter.use(`${BASE_URL}/oauth`, oAuthRoutes);
 appRouter.use(`${BASE_URL}/users`, userRoutes);
 appRouter.use(`${BASE_URL}/doctors`, doctorRoutes);
 appRouter.use(`${BASE_URL}/appointments`, appointmentRoutes);
@@ -30,6 +29,7 @@ appRouter.use(`${BASE_URL}/countries`, countryRoutes);
 appRouter.use(`${BASE_URL}/hospitals`, hospitals);
 appRouter.use(`${BASE_URL}/super-doctor`, superDoctorRoutes);
 appRouter.use(`${BASE_URL}/payments`, paymentRoutes);
+appRouter.use(`${BASE_URL}/chats`, chatRoutes);
 
 
 appRouter.get("/", (req, res) => {
