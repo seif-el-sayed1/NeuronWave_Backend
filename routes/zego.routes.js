78572229a808cd5bf581167c643c895677f3252a
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-20 13:50:03 +0200

    generate zego token route

---
+++ routes/zego.routes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { generateZegoToken } = require("../controllers/zego.controller");
+const { protect } = require("../middlewares/auth.middleware");
+
+
+router.post("/token", protect, generateZegoToken);
+
+module.exports = router;
