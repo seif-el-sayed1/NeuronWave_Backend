c800572d7e53bf08c3a648aac9968bf3938d0892
seif-el-sayed1 <seifsayed121@gmail.com>
2026-03-01 02:06:51 +0200

    fix: remove unnecessary blank line in zego.routes.js

--- routes/zego.routes.js
+++ routes/zego.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { generateZegoToken } = require("../controllers/zego.controller");
+const { generateZegoTokenFlutter } = require("../controllers/zego.controller");
 const { protect } = require("../middlewares/auth.middleware");
-
 
 router.post("/token", protect, generateZegoToken);
 
